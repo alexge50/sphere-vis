6a7699402795ad3ead23f0f075ae26b73386f1d2
alexge50 <alexge50.x@gmail.com>
2019-11-01 01:59:25 +0200

    sphere rendering code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/mat4x4.hpp>
+#include <glm/common.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 #include <fstream>
 
 #include <Shader.h>
+#include <Sphere.h>
 
 std::string loadFile(const char* name)
 {
@@ -15,6 +22,10 @@ std::string loadFile(const char* name)
         std::istreambuf_iterator<char>{}
     };
 }
+
+const float RADIUS = 10.f;
+const int RINGS = 10;
+const int SECTORS = 10;
 
 int main()
 {
@@ -50,6 +61,54 @@ int main()
         loadFile("shaders/vertex.glsl"),
         loadFile("shaders/fragment.glsl")
     ).value();
+    const auto mvpLocation = shader.getUniformLocation("mvp");
+
+    Sphere sphere = generateSphere(RADIUS, RINGS, SECTORS);
+
+    unsigned int vbo = 0;
+    unsigned int ebo = 0;
+    unsigned int vao = 0;
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sphere.vertices.size() * sizeof(float),
+        sphere.vertices.data(),
+        GL_DYNAMIC_DRAW
+    );
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(
+            GL_ELEMENT_ARRAY_BUFFER,
+            sphere.indices.size() * sizeof(int),
+            sphere.indices.data(),
+            GL_STATIC_DRAW
+    );
+
+    glGenVertexArrays(1, &vao);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        3 * sizeof(float),
+        nullptr
+    );
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glEnable(GL_DEPTH_TEST);
 
     while(!glfwWindowShouldClose(window))
     {
@@ -59,9 +118,31 @@ int main()
         glfwGetFramebufferSize(window, &width, &height);
         ratio = float(width) / float(height);
         glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-
         glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        auto mvp = glm::perspective(
+                0.78f,
+                ratio,
+                0.00001f,
+                100.f
+        ) * glm::lookAt(
+                glm::vec3(0.f, 0.f, 40.f),
+                glm::vec3(0.f, 0.f, 0.f),
+                glm::vec3(0.f, 1.f, 0.f)
+        );
+
+        shader.use();
+        glUniformMatrix4fv(mvpLocation, 1, 0, glm::value_ptr(mvp));
+
+        glBindVertexArray(vao);
+        glDrawElements(
+            GL_LINE_STRIP,
+            sphere.indices.size(),
+            GL_UNSIGNED_INT,
+            nullptr
+        );
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
